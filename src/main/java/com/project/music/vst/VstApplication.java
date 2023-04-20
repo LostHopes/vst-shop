14c4e86f8827f00c6de3dea061b507a0f7d9a869
losthope <arsen.melnuchyk@gmail.com>
2023-04-20 22:45:58 +0300

    init project

---
+++ src/main/java/com/project/music/vst/VstApplication.java
@@ -0,0 +1,13 @@
+package com.project.music.vst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/music/vst/VstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.music.vst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
